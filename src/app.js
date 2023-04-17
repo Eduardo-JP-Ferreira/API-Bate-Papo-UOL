acbae82a339b68a125f2f4c9b1630aee297e2e7b
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-04-17 06:45:10 -0300

    Implemento POST messages

--- src/app.js
+++ src/app.js
@@ -26,11 +26,12 @@ app.post("/participants",async (req, res) => {
     const nomeParticipante = joi.object({
         name: joi.string().required()
       })
-    const validate = nomeParticipante.validate(req.body)
-    if (validate.error) return res.sendStatus(422)
-    // const verificaNome = db.collection("participants").find().toArray()
-    // const pessoa = verificaNome.find((verificaNome)=> verificaNome.name === name)
-    // res.send(name)
+    const validate = nomeParticipante.validate(req.body, { abortEarly: false })
+    if (validate.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+      }
+
     const verificaNome= await db.collection("participants").findOne({ name: name })
     if(!verificaNome){
         try{
@@ -65,6 +66,48 @@ app.get("/participants", async(req, res) => {
     const participantes = await db.collection("participants").find().toArray()
         .then((participantes) => res.status(200).send(participantes))
         .catch((err) => res.status(500).send(err.message))
+})
+
+app.post("/messages",async (req, res) => {
+    const {to, text, type} = req.body
+    const {user} = req.headers
+
+    const nome = "marta"
+    const mensagemPost = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().required()
+      })
+    const validate = mensagemPost.validate(req.body, { abortEarly: false })
+    if (validate.error) {
+        const errors = validate.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+      }
+    if(type !== 'message' && type !== 'private_message') return res.sendStatus(422)
+   
+    const verificaNome= await db.collection("participants").findOne({ name: user })
+    if(verificaNome && user !== undefined){
+        try{
+    
+            const hora = dayjs().format('HH:mm:ss')
+            const mensagem = {
+                from: user,
+                to,
+                text,
+                type,
+                time: hora
+            }
+    
+            await db.collection("messages").insertOne(mensagem);
+            return res.sendStatus(201)
+        }catch (err){
+            console.log(err);
+            res.sendStatus(422);
+          }
+    }else{
+        res.status(422).send(`try  ${user}`);
+    }
+    
 })
 
 app.get("/messages",async (req, res) => {
@@ -103,13 +146,7 @@ app.get("/messages",async (req, res) => {
         }
     }
 
-   
-
-
-
-
 })
-
 
 
 
